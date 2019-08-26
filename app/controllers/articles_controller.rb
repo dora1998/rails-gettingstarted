395a00e810a724b2d753bf84141607b13e566fd1
dora <31735614+dora1998@users.noreply.github.com>
2019-08-27 00:46:53 +0900

    Create Article model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,25 @@
 class ArticlesController < ApplicationController
-  def new
+  def index
+    @articles = Article.all
+  end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
   end
 
   def create
-    @article  = Article.new(params.require(:article).permit(:title, :text))
+    @article = Article.new(article_params)
 
     @article.save
+    binding.pry
     redirect_to @article
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
